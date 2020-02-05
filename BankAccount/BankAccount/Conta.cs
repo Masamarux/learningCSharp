6253215f123edb77c309d65196743de31f2e63ea
Marcelo Alves <alves-marcelo97@hotmail.com>
2020-02-05 17:01:36 -0300

    adding a simple bank account system using oop

---
+++ BankAccount/BankAccount/Conta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount {
+    class Conta {
+
+        private string _nome;
+        public int NumeroConta { get; set; }
+        public double Valor { get; private set; }
+
+        public Conta(string nome, int numeroConta, double valor){
+            _nome = nome;
+            NumeroConta = numeroConta;
+            Valor = valor;
+        }
+
+        public string Nome {
+            get { return _nome; }
+            set {
+                if (value != null && value.Length > 1) {
+                    _nome = value;
+                }
+                else {
+                    Console.WriteLine("Você infomou um nome inválido");//colocar timer para fechar a aplicação
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace BankAccount {
+    class Program {
+        static void Main(string[] args) {
+            int numeroConta;
+            string nome;
+            double valor;
+            char inicial;
+
+            Console.Write("Entre o número da conta: ");
+            numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)?");
+            inicial = char.Parse(Console.ReadLine());
+            if (inicial == 's') {
+                Console.Write("Entre o valor do depósito inicial: ");
+                valor = double.Parse(Console.ReadLine());
+            }
+            else {
+                valor = 0.00;
+            }
+            Conta acc = new Conta(nome, numeroConta, valor);
+
+            Console.WriteLine("Dados da conta: ");//fazer tostring override na classe
+
+            Console.Write("Entre um valor para depósito: ");
+            Console.ReadLine();
+            Console.WriteLine("Dados da conta atualizados: ");//novamente com o toString
+
+            Console.Write("Entre um valor para saque: ");
+            Console.ReadLine();
+            Console.WriteLine("Dados da conta atualizados: ");//novamente com o toString
+        }
+    }
+}
