e4e79f1d99e6ffda0294d54530cd5157d9e401e2
Marcelo Alves <alves-marcelo97@hotmail.com>
2020-02-06 15:59:41 -0300

    exercise using lists

---
+++ Using Vectors, Matrices and Arrays/EmployeeRegister/EmployeeRegister/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace EmployeeRegister {
+    class Employee {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary) {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentage) {
+            Salary += (Salary * percentage) / 100;
+        }
+
+        public override string ToString() {
+            return  Id
+                    + ", "
+                    + Name
+                    + ", R$ "
+                    + Salary.ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Using Vectors, Matrices and Arrays/EmployeeRegister/EmployeeRegister/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace EmployeeRegister {
+    class Program {
+        static void Main(string[] args) {
+            int manyEmployees, id, idSearch;
+            string name;
+            double salary;
+            List<Employee> listEmployees = new List<Employee>();
+
+            Console.Write("How many employees will be registered? ");
+            manyEmployees = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < manyEmployees; i++) {
+                Console.WriteLine($"Employee #{i+1}:");
+
+                Console.Write("Id: ");
+                id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Employee emp = new Employee(id, name, salary);
+                listEmployees.Add(emp);
+                Console.WriteLine();//breakline
+            }
+
+            Console.Write("Enter the employee id that will have salary increase: ");
+            idSearch = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            FindAndIncreaseSalary(listEmployees, idSearch);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees: ");
+            foreach (Employee employee in listEmployees) {
+                Console.WriteLine(employee);
+            }
+
+
+        }
+
+        static void FindAndIncreaseSalary(List<Employee> listEmployees, int idSearch ) {
+            if (listEmployees.Exists(x => x.Id == idSearch)) {
+                int indexOf;
+                double percentage;
+
+                indexOf = listEmployees.FindIndex(x => x.Id == idSearch);
+
+                Console.Write("Enter the percentage: ");
+                percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                listEmployees[indexOf].IncreaseSalary(percentage);
+            }
+            else {
+                Console.WriteLine("This id not exists!");
+            }
+        }
+    }
+}
