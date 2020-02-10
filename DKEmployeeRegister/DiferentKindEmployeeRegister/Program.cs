ff15f1f41c65db18f021728b8f5514e6209546b8
Marcelo Alves <alves-marcelo97@hotmail.com>
2020-02-10 17:00:41 -0300

    building employee register w enheritance

---
+++ DKEmployeeRegister/DiferentKindEmployeeRegister/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DiferentKindEmployeeRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nEmployees, hours;
+            char outsourced;
+            string name;
+            double valuePerHour, additionalCharge;
+
+            Console.WriteLine("Enter the number of employees: ");
+            nEmployees = int.Parse(Console.ReadLine());
+            for(int i = 0; i < nEmployees; i++)
+            {
+                outsourced = ' ';
+                Console.WriteLine($"Employee #{i} data:");
+                Console.WriteLine("Outsourced (y/n)? ");
+                outsourced = char.Parse(Console.ReadLine());
+                if(outsourced == 'y' || outsourced == 'Y')
+                {
+                    Console.WriteLine("Name: ");
+                    name = Console.ReadLine();
+                    Console.WriteLine("Hours: ");
+                    hours = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Value per hour:");
+                    valuePerHour = double.Parse(Console.ReadLine());
+
+                }
+
+            }
+        }
+    }
+}
